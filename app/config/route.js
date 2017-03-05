4bdbce7224b7820a2ac488428338d6d754f4010b
wu kai <wukai2048@gmail.com>
2017-03-05 11:20:30 +0800

    use  hashHistory

--- app/config/route.js
+++ app/config/route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router';
 import Main from '../containers/Main';
 import DetailContainer from './../containers/DetailContainer';
 import StoryListContainer from './../containers/StoryListContainer';
@@ -7,7 +7,7 @@ import ThemeContainer from './../containers/ThemeContainer';
 
 
 const route = (
-    <Router history={browserHistory} key={Math.random()}>
+    <Router history={hashHistory} key={Math.random()}>
         <Route path="/" component={Main}>
             <IndexRoute component={StoryListContainer} />
             <Route path="detail/:id" component={DetailContainer} />
